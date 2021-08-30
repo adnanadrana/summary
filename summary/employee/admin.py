d8d09ef15a403788f50150bc6111b45a93e3807f
ADNANALI-PC\Adnan Ali <adnanadrana@gmail.com>
2021-08-30 21:32:18 +0500

    EmployeeDepartmentModulesAddedd

--- summary/employee/admin.py
+++ summary/employee/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Employee, Department
+# from simple_history.admin import SimpleHistoryAdmin
 
 # Register your models here.
+
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'department']
+    list_filter = ['department']
+
+
+
+class DepartmentAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Employee, EmployeeAdmin)
+admin.site.register(Department, DepartmentAdmin)
+
+
